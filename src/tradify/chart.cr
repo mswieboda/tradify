f627f9002fb71db950ae92c78f4eed11c3f9065b
Matt Swieboda <matts@topsteptrader.com>
2019-04-12 18:50:15 -0500

    added auto plot point and lines

--- src/tradify/chart.cr
+++ src/tradify/chart.cr
@@ -1,12 +1,24 @@
 module Tradify
   class Chart
     BACKGROUND_COLOR = LibRay::Color.new(r: 30, g: 30, b: 30, a: 0)
-    LINE_COLOR       = LibRay::Color.new(r: 0, g: 255, b: 0, a: 30)
+    GRID_COLOR       = LibRay::Color.new(r: 0, g: 255, b: 0, a: 30)
+    PLOT_COLOR       = LibRay::GREEN
 
     GRID_SIZE      = 50
     GRID_THICKNESS =  1
 
+    POINT_SIZE           =   4
+    LINE_THICKNESS       =   4
+    LINE_THICKNESS_RATIO = 2.0
+
+    POINTS = [50, 70, 90, 40, 60, 30, 80, 100, 130, 150, 130, 110, 100, 90, 110, 130, 120, 110, 90, 70, 50, 60, 50, 40, 50, 70, 60]
+
+    DATA_INTERVAL = 0.5
+
     def initialize(@x : Int32, @y : Int32, @width : Int32, @height : Int32)
+      @timer = Timer.new(DATA_INTERVAL)
+      @point_index = 0
+      @points = [] of Int32
     end
 
     def initialize
@@ -14,11 +26,22 @@ module Tradify
     end
 
     def update
+      delta_t = LibRay.get_frame_time
+
+      @timer.increase(delta_t)
+
+      if @timer.done?
+        @points << POINTS[@point_index]
+        @point_index += 1
+        @point_index = 0 if @point_index >= POINTS.size
+        @timer.restart
+      end
     end
 
     def draw
       draw_background
       draw_grid
+      draw_plot
     end
 
     def draw_background
@@ -41,7 +64,7 @@ module Tradify
             x: GRID_THICKNESS,
             y: @height
           ),
-          color: LINE_COLOR
+          color: GRID_COLOR
         )
       end
 
@@ -56,8 +79,50 @@ module Tradify
             x: @width,
             y: GRID_THICKNESS
           ),
-          color: LINE_COLOR
+          color: GRID_COLOR
         )
+      end
+    end
+
+    def draw_plot
+      last_px = @x - POINT_SIZE / 2
+      last_py = @y + @height / 2 - POINT_SIZE / 2
+
+      @points.each_with_index do |point, point_x|
+        px = @x + point_x * GRID_SIZE / 2 - POINT_SIZE / 2
+        py = @y + @height / 2 + point - POINT_SIZE / 2
+
+        # point
+        LibRay.draw_rectangle_v(
+          position: LibRay::Vector2.new(x: px, y: py),
+          size: LibRay::Vector2.new(x: POINT_SIZE, y: POINT_SIZE),
+          color: PLOT_COLOR
+        )
+
+        # line
+        LibRay.draw_line_v(
+          start_pos: LibRay::Vector2.new(x: last_px, y: last_py),
+          end_pos: LibRay::Vector2.new(x: px, y: py),
+          color: PLOT_COLOR
+        )
+
+        # thickness
+        LINE_THICKNESS.times do |thickness|
+          # puts "t: #{thickness}"
+          LibRay.draw_line_v(
+            start_pos: LibRay::Vector2.new(x: last_px, y: last_py + thickness / LINE_THICKNESS_RATIO),
+            end_pos: LibRay::Vector2.new(x: px, y: py + thickness / LINE_THICKNESS_RATIO),
+            color: PLOT_COLOR
+          )
+          LibRay.draw_line_v(
+            start_pos: LibRay::Vector2.new(x: last_px, y: last_py - thickness / LINE_THICKNESS_RATIO),
+            end_pos: LibRay::Vector2.new(x: px, y: py - thickness / LINE_THICKNESS_RATIO),
+            color: PLOT_COLOR
+          )
+        end
+
+        last_px = px
+        last_py = py
       end
     end
   end
