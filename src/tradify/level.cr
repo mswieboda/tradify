ee2f10661833ea6f4231df11ba4598183af018ff
Matt Swieboda <mswiebod@gmail.com>
2019-04-13 00:12:10 -0500

    added target profit

--- src/tradify/level.cr
+++ src/tradify/level.cr
@@ -1,19 +1,24 @@
 module Tradify
   class Level
     getter? loaded
+    getter? completed
 
     @screen : Screen
 
-    PRICE_DATA = [50, 70, 90, 40, 60, 30, 80, 100, 130, 150, 130, 110, 100, 90, 110, 130, 120, 110, 90, 70, 50, 60, 50, 40, 50, 70, 60]
+    INITIAL_BALANCE = 1000
+    TARGET_PROFIT   =  100
+    PRICE_DATA      = [50, 70, 90, 40, 60, 30, 80, 100, 130, 150, 130, 110, 100, 90, 110, 130, 120, 110, 90, 70, 50, 60, 50, 40, 50, 70, 60]
 
     def initialize(@game : Game)
       @loaded = false
-      @account = Account.new(balance: 1000)
+      @account = Account.new(balance: INITIAL_BALANCE)
       @screen = Screen.new(
         game: @game,
         account: @account,
         price_data: PRICE_DATA
       )
+
+      @completed = false
     end
 
     def load
@@ -32,6 +37,12 @@ module Tradify
     def update
       @screen.update
 
+      if !completed? && target_reached?
+        message = TypedMessage.new(["Congrats, you made a profit of $#{TARGET_PROFIT}!"])
+        @game.show(message)
+        @completed = true
+      end
+
       return if loaded?
 
       start
@@ -39,8 +50,8 @@ module Tradify
       @loaded = true
     end
 
-    def complete?
-      false
+    def target_reached?
+      @account.balance >= INITIAL_BALANCE + TARGET_PROFIT
     end
   end
 end
